2b8f6c4b539d1eab24b512512831cf3a72aa77c1
bin1225 <gimgyubin1225@gmail.com>
2024-03-18 14:15:07 +0900

    refactor: 폴더 구조 변경 src -> src\main

--- src/main/BaseballGameController.java
+++ src/main/BaseballGameController.java
@@ -1,3 +1,5 @@
+package main;
+
 public class BaseballGameController {
 
     private static Ball ball = new Ball();
--- src/main/Output.java
+++ src/main/Output.java
@@ -1,8 +1,14 @@
+package main;
+
 public class Output {
 
     private static final String GAME_START_MESSAGE = "숫자야구 게임을 시작합니다.";
+    private static final String INPUT_GUIDE_MESSAGE = "서로 다른 숫자 3개를 입력하세요.";
     public static void printGameStartMessage(){
         System.out.println(GAME_START_MESSAGE);
     }
 
+    public static void printInputGuideMessage(){
+        System.out.println(INPUT_GUIDE_MESSAGE);
+    }
 }
