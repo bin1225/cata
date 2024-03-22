7bdf82e6fede5687a5ce61eb255f2480f038769e
bin1225 <gimgyubin1225@gmail.com>
2024-03-22 18:44:32 +0900

    feat: Hint 출력 기능 추가 게임 종료 메시지 출력

--- src/main/Output.java
+++ src/main/Output.java
@@ -5,6 +5,7 @@ public class Output {
     private static final String GAME_START_MESSAGE = "숫자야구 게임을 시작합니다.";
     private static final String INPUT_GUIDE_MESSAGE = "서로 다른 숫자 3개를 입력하세요.";
     private static final String INPUT_ERROR_MESSAGE = "잘못된 입력입니다. 처음부터 다시 입력해주세요.";
+    private static final String GAME_END_MESSAGE = "게임이 종료됩니다. 수고하셨습니다.";
     public static void printGameStartMessage(){
         System.out.println(GAME_START_MESSAGE);
     }
@@ -14,5 +15,12 @@ public class Output {
     }
     public static void printInputErrorMessage(){
         System.out.println(INPUT_ERROR_MESSAGE);
+    }
+    public static void printGameEndMessage(){
+        System.out.println(GAME_END_MESSAGE);
+    }
+
+    public static void printHint(Hint hint){
+        System.out.println(hint.toString());
     }
 }
