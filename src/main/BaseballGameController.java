52384022de3843c5a8d39ee5ad86ab84dcb9fd1c
bin1225 <gimgyubin1225@gmail.com>
2024-03-26 16:04:49 +0900

    feat: 게임 실행 흐름 관리 기능 추가 게임 시작 및 종료 메시지 출력 호출 정답을 맞출 때까지 입력과 힌트 출력 반복

--- src/main/BaseballGameController.java
+++ src/main/BaseballGameController.java
@@ -1,12 +1,23 @@
 package main;
 
+import java.util.List;
+
 public class BaseballGameController {
 
-    private static Ball ball = new Ball();
+    private static Computer computer = new Computer();
     public static void startGame(){
         Output.printGameStartMessage();
-        ball.createBall();
+        List<Integer> expectNumbers = computer.createRandomNumbers();
+
         Output.printInputGuideMessage();
-        Input.getThreeNumber();
+        List<Integer> inputNumbers = Input.getThreeNumber();
+
+        while(true){
+            Hint hint = computer.compareNumbers(expectNumbers, inputNumbers);
+            if(hint.getStrike() == GameConfiguration.BALL_SIZE) break;
+            Output.printHint(hint);
+            inputNumbers = Input.getThreeNumber();
+        }
+        Output.printGameEndMessage();
     }
 }
